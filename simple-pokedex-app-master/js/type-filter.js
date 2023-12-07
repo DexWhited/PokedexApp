8806019fe1c729897745d6b85899e9b256f249a9
cwhited <cwhited@student.neumont.edu>
2023-12-06 21:33:39 -0700

    type filter with images

--- simple-pokedex-app-master/js/type-filter.js
+++ simple-pokedex-app-master/js/type-filter.js
@@ -1,18 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelectorAll('.grid-item').forEach(function (item) {
         item.addEventListener('click', function () {
-            // Fetch and display Pokemon of the respective type using the ID
-            var typeId = item.id.split('-')[2]; // Extract the type ID from the element ID
-            fetchAndDisplayPokemonByType(typeId);
+            // Toggle the visibility of the Pokemon list and details
+            var typeId = item.id.split('-')[2]; 
+            togglePokemonDetails(typeId);
         });
     });
 });
 
-// Function to fetch and display Pokemon by type
-function fetchAndDisplayPokemonByType(typeId) {
-    // Use the typeId to construct the API URL
+function togglePokemonDetails(typeId) {
     var apiUrl = 'https://pokeapi.co/api/v2/type/' + typeId;
-
 
     fetch(apiUrl)
         .then(function (response) {
@@ -22,24 +19,69 @@ function fetchAndDisplayPokemonByType(typeId) {
             return response.json();
         })
         .then(function (data) {
-            // update the display
-            updatePokemonList(data.pokemon);
+            // Toggle the visibility of the Pokemon list and details
+            toggleListAndDetails(data.pokemon);
         })
         .catch(function (error) {
             console.error('Error fetching Pokemon data:', error.message);
         });
 }
 
-// Function to update the Pokemon list display
-function updatePokemonList(pokemonArray) {
-    
+function toggleListAndDetails(pokemonArray) {
     var pokemonList = document.querySelector('.pokemon-list');
-    pokemonList.innerHTML = ''; // Clear existing list
+    var existingListItems = document.querySelectorAll('.pokemon-list-item');
 
-    // Add each Pokemon to the list
-    pokemonArray.forEach(function (pokemon) {
-        var listItem = document.createElement('li');
-        listItem.textContent = pokemon.pokemon.name;
-        pokemonList.appendChild(listItem);
-    });
+    // Check if the list is already populated
+    if (existingListItems.length === 0) {
+        // If not, add each Pokemon to the list
+        pokemonArray.forEach(function (pokemon) {
+            var listItem = document.createElement('li');
+            listItem.textContent = pokemon.pokemon.name;
+            listItem.classList.add('pokemon-list-item');
+
+            // Add click event to toggle the visibility of the details
+            listItem.addEventListener('click', function () {
+                toggleDetailsVisibility(listItem);
+            });
+
+            pokemonList.appendChild(listItem);
+        });
+    } else {
+        // If the list is populated, clear it
+        pokemonList.innerHTML = '';
+    }
+}
+
+function toggleDetailsVisibility(listItem) {
+    // Toggle the visibility of the details (image)
+    var imageElement = listItem.querySelector('img');
+    if (imageElement) {
+        // If the image is present, remove it
+        listItem.removeChild(imageElement);
+    } else {
+        var pokemonName = listItem.textContent;
+        fetchPokemonDetails(pokemonName, listItem);
+    }
+}
+
+function fetchPokemonDetails(pokemonName, listItem) {
+    var detailsUrl = 'https://pokeapi.co/api/v2/pokemon/' + pokemonName;
+
+    fetch(detailsUrl)
+        .then(function (response) {
+            if (!response.ok) {
+                throw new Error('Error fetching Pokemon details');
+            }
+            return response.json();
+        })
+        .then(function (details) {
+            // Add the image to the list item
+            var imageUrl = details.sprites.front_default;
+            var imageElement = document.createElement('img');
+            imageElement.src = imageUrl;
+            listItem.appendChild(imageElement);
+        })
+        .catch(function (error) {
+            console.error('Error fetching Pokemon details:', error.message);
+        });
 }
