4d1b4da9d86ad71b6482d1a6cd61b5a487714899
cwhited <cwhited@student.neumont.edu>
2023-12-06 19:55:51 -0700

    search

--- simple-pokedex-app-master/js/scripts.dist.js
+++ simple-pokedex-app-master/js/scripts.dist.js
@@ -1,170 +1,184 @@
-'use strict';
-
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
-
-var pokemonRepository = function () {
-    var items = [];
-    //current url below brings up all pokemon up to ogerpon
-    var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=1017';
-
-    // url below brings up all extra pokemon after ogerpon | deoxys-attack, deoxys-defense etc.
-    // after ogerpon, list goes back to pokedex order of different forms/variations that is where
-    // you find different forms (alolan, galarian, hisuian, paldean | mega forms, g-max, variants).
-    // original hisui pokemon (Kleavor, Overqwil, Enamorus) will be found after galar pokemon and before paldean pokemon
-    // var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=100000';
-
+let pokemonRepository = (function() {
+    let items = [];
+    // define a new list to store the searched pokemon
+    let new_list = [];
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  
     function addSingle(item) {
-        if (validate(item)) {
-            items.push(item);
-        } else {
-            console.error('Error when validating item', item);
-        }
+      if (validate(item)) {
+        items.push(item);
+      } else {
+        /* eslint-disable no-console */
+        console.error('Error when validating item', item);
+        /* eslint-enable no-console */
+      }
     }
-
+  
     function add(item) {
-        if (Array.isArray(item)) {
-            items.forEach(addSingle);
-        } else {
-            addSingle(item);
-        }
+      if (Array.isArray(item)) {
+        items.forEach(addSingle);
+      } else {
+        addSingle(item);
+      }
     }
-
+  
     function validate(item) {
-        return (typeof item === 'undefined' ? 'undefined' : _typeof(item)) === 'object' && item.name;
+      return typeof item === 'object' && item.name;
     }
-
+  
     function getAll() {
-        return items;
+      return items;
     }
-
+  
     function remove(item) {
-        var itemPos = items.indexOf(item);
-        if (itemPos > -1) {
-            items.splice(itemPos, 1);
-        }
+      let itemPos = items.indexOf(item);
+      if (itemPos > -1) {
+        items.splice(itemPos, 1);
+      }
     }
-
+  
     function search(str) {
-        str = str.toLowerCase(); // We want to compare lower case strings
-        return items.filter(function (item) {
-            return item.name.toLowerCase().indexOf(str) > -1;
-        });
+        // new list and set it to be filter of hitems with tsearch or str
+        new_list = items.filter(pokemon => pokemon.name.startsWith(str))
+
     }
 
+    document.addEventListener('DOMContentLoaded', function () {
+        searchInput.addEventListener('input', function () {
+            const inputValue = searchInput.value.trim().toLowerCase();
+            search(inputValue);
+            console.log(new_list);
+
+            updateList(); 
+        });
+    });
+
+
+
+    function updateList(){
+        let container = document.querySelector('.pokemon-list');
+        container.innerHTML = '';
+        new_list.forEach(addListItem);
+
+    }
+
+  
     function loadList() {
-        showLoadingMessage();
-        return fetch(apiUrl).then(function (response) {
-            // response.ok checks if the status === 200
-            if (!response.ok) {
-                return Promise.reject('Error loading data'); // This will end in the .catch block
-            }
-            return response.json();
-        }).then(function (json) {
-            json.results.forEach(function (item) {
-                var pokemon = {
-                    name: item.name,
-                    detailsUrl: item.url
-                };
-                add(pokemon);
-            });
-            hideLoadingMessage();
-        }).catch(function (e) {
-            console.error(e);
-            hideLoadingMessage();
+      showLoadingMessage();
+      return fetch(apiUrl)
+        .then(function(response) {
+          // response.ok checks if the status === 200
+          if (!response.ok) {
+            return Promise.reject('Error loading data'); // This will end in the .catch block
+          }
+          return response.json();
+        })
+        .then(function(json) {
+          json.results.forEach(function(item) {
+            let pokemon = {
+              name: item.name,
+              detailsUrl: item.url
+            };
+            add(pokemon);
+          });
+          hideLoadingMessage();
+        })
+        .catch(function(e) {
+          console.error(e); // eslint-disable-line no-console
+          hideLoadingMessage();
         });
     }
-
+  
     function loadDetails(item) {
-        var url = item.detailsUrl,
-            imageUrl = item.imageUrl;
+      let { detailsUrl: url, imageUrl } = item;
 
-        // If an image is already loaded, we don't need to fetch it again
-        // We still want to return a Promise, as otherwise the .then will not work
-
-        if (imageUrl) {
-            return Promise.resolve(); // This returns a new Promise that resolves immediately
-        }
-
-        showLoadingMessage();
-        return fetch(url).then(function (response) {
-            if (!response.ok) {
-                return Promise.reject('Error loading data'); // This will end in the .catch block
-            }
-            return response.json();
-        }).then(function (details) {
-            // Now we add the details to the item
-            item.imageUrl = details.sprites.front_default;
-            hideLoadingMessage();
-        }).catch(function (e) {
-            console.error(e);
-            hideLoadingMessage();
+      if (imageUrl) {
+        return Promise.resolve(); // This returns a new Promise that resolves immediately
+      }
+  
+      showLoadingMessage();
+      return fetch(url)
+        .then(function(response) {
+          if (!response.ok) {
+            return Promise.reject('Error loading data'); // This will end in the .catch block
+          }
+          return response.json();
+        })
+        .then(function(details) {
+          // Now we add the details to the item
+          item.imageUrl = details.sprites.front_default;
+          hideLoadingMessage();
+        })
+        .catch(function(e) {
+          console.error(e); // eslint-disable-line no-console
+          hideLoadingMessage();
         });
     }
-
+  
     return {
-        add: add,
-        getAll: getAll,
-        remove: remove,
-        search: search,
-        loadList: loadList,
-        loadDetails: loadDetails
+      add,
+      getAll,
+      remove,
+      search,
+      loadList,
+      loadDetails
     };
-}();
-
-function addListItem(item) {
-    var container = document.querySelector('.pokemon-list');
-    var listItem = document.createElement('li');
-    listItem.classList.add('pokemon-list-item');
-
-    var title = document.createElement('div');
+  })();
+  
+  function addListItem(item) {
+    let container = document.querySelector('.pokemon-list');
+    let listItem = document.createElement('li');
+  
+    let title = document.createElement('div');
     title.innerText = item.name;
     listItem.appendChild(title);
-
+  
     container.appendChild(listItem);
-
-    listItem.addEventListener('click', function () {
-        showDetails(item, listItem);
+  
+    listItem.addEventListener('click', () => {
+      showDetails(item, listItem);
     });
-}
-
-function showDetails(item, listItem) {
-    var listItemDetails = listItem.querySelector('.details');
-
+  }
+  
+  function showDetails(item, listItem) {
+    let listItemDetails = listItem.querySelector('.details');
+  
     // If details already exist, remove them
     if (listItemDetails) {
-        listItem.removeChild(listItemDetails);
+      listItem.removeChild(listItemDetails);
     } else {
-        // Else, first load the details, then add the element
-        pokemonRepository.loadDetails(item).then(function () {
-            var imageUrl = item.imageUrl;
-
-            listItemDetails = document.createElement('img');
-            listItemDetails.setAttribute('src', imageUrl);
-            listItemDetails.classList.add('details');
-            listItem.appendChild(listItemDetails);
-        });
+      // Else, first load the details, then add the element
+      pokemonRepository.loadDetails(item).then(function() {
+        let imageUrl = item.imageUrl;
+  
+        listItemDetails = document.createElement('img');
+        listItemDetails.setAttribute('src', imageUrl);
+        listItemDetails.classList.add('details');
+        listItem.appendChild(listItemDetails);
+      });
     }
-}
-
-function showLoadingMessage() {
-    var container = document.querySelector('main');
-
-    var loadingMessage = document.createElement('div');
+  }
+  
+  function showLoadingMessage() {
+    let container = document.querySelector('main');
+  
+    let loadingMessage = document.createElement('div');
     loadingMessage.classList.add('loading-message');
     loadingMessage.innerText = 'Loading...';
-
+  
     container.insertBefore(loadingMessage, container.firstElementChild);
-}
-
-function hideLoadingMessage() {
-    var loadingMessage = document.querySelector('.loading-message');
+  }
+  
+  function hideLoadingMessage() {
+    let loadingMessage = document.querySelector('.loading-message');
     loadingMessage.parentElement.removeChild(loadingMessage);
-}
-
-pokemonRepository.loadList().then(function () {
+  }
+  
+  pokemonRepository.loadList().then(function() {
     // Now the data is loaded!
-    var allPokemon = pokemonRepository.getAll();
-    allPokemon.forEach(function (item) {
-        addListItem(item);
+    let allPokemon = pokemonRepository.getAll();
+    allPokemon.forEach(function(item) {
+      addListItem(item);
     });
-});
+  });
+  
